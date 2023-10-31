56c04ff6f8e6339f2ce8e7af2b9f2d94577c38c8
NafisOfficial <nafisiqbal.net2002@gmail.com>
2023-10-31 19:42:40 +0600

    Our menu cover done

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
 import menuBanner from '../../../assets/menu/banner3.jpg'
+import Sectiontitle from '../../../Components/Sectiontitle/Sectiontitle';
+import PopularMenu from '../../Home/PopularMenu/PopularMenu';
+
 
 const Menu = () => {
     return (
@@ -10,6 +12,8 @@ const Menu = () => {
                 <title>Bistro Boss || Menu</title>
             </Helmet>
             <Cover bgImg={menuBanner} title="Our menu" subtitle='WOULD YOU LIKE TO TRY A DISH'></Cover>
+            <Sectiontitle subheading="Don't miss" heading="Today's offer"></Sectiontitle>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
--- src/Pages/Shared/Cover/Cover.jsx
+++ src/Pages/Shared/Cover/Cover.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import { Parallax} from 'react-parallax';
 
-const Cover = ({bgImg,title,subtitle}) => {
+const Cover = ({ bgImg, title, subtitle }) => {
     return (
         <div>
-            <div className="hero h-[500px]" style={{ backgroundImage: `url(${bgImg})` }}>
-                <div className="hero-overlay bg-opacity-60"></div>
-                <div className="hero-content text-center text-neutral-content">
-                    <div className="max-w-md">
-                        <h1 className="mb-5 text-5xl font-bold">{title}</h1>
-                        <p className="mb-5">{subtitle}</p>
+            <Parallax
+                blur={{ min: -50, max: 50 }}
+                bgImage={bgImg}
+                bgImageAlt="Our menu"
+                strength={-200}
+            >
+                <div className="hero h-[500px]">
+                    <div className="hero-overlay bg-opacity-60"></div>
+                    <div className="hero-content text-center text-neutral-content">
+                        <div className="max-w-md">
+                            <h1 className="mb-5 text-5xl font-bold">{title}</h1>
+                            <p className="mb-5">{subtitle}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Parallax>
+
         </div>
     );
 };
