b40ebf3fd250532d0314c0e215dbe76663430a4a
NafisOfficial <nafisiqbal.net2002@gmail.com>
2023-11-05 02:05:37 +0600

    login page design complete

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,34 +1,47 @@
 import React from 'react';
-
+import item from '../../assets/others/authentication2.png'
+import './login.css'
+import { Link } from 'react-router-dom';
+import { BsGoogle } from 'react-icons/bs';
+import { PiFacebookLogo } from 'react-icons/pi';
+import { BsGithub} from 'react-icons/bs';
 const Login = () => {
     return (
-        <div>
-            <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                    <div className="text-center lg:text-left">
-                        <h1 className="text-5xl font-bold">Login now!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    </div>
-                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+        <div className='bg1'>
+            <div className="hero min-h-screen">
+                <div className="hero-content flex-col">
+                    <div className="flex gap-10 items-center shadow-2xl bg2 px-48 py-12">
+                        <div className=''>
+                            <img className='w-72 h-56'  src={item} alt="" />
+                        </div>
                         <form className="card-body">
+                            <h3 className='text-center font-bold text-4xl'>Login</h3>
                             <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">Email</span>
+                                    <span className="label-text font-bold">Email</span>
                                 </label>
-                                <input type="email" placeholder="email" className="input input-bordered" required />
+                                <input type="email" placeholder="Type here" className="input bg-white " required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">Password</span>
+                                    <span className="label-text font-bold">Password</span>
                                 </label>
-                                <input type="password" placeholder="password" className="input input-bordered" required />
-                                <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                </label>
+                                <input type="password" placeholder="Enter your password" className="input bg-white" required />
+                            </div>
+                            <div className="form-control">
+                                <input type="text" placeholder="Enter captcha" className="input bg-white" required />
                             </div>
                             <div className="form-control mt-6">
-                                <button className="btn btn-primary">Login</button>
+                                <button className="btn bg-[#FDDAA8] text-white">Sign in</button>
                             </div>
+                            
+                        <p className='text-[#cc9d5a]'>New here? Create a new account ! <Link className='text-blue-700'>Click here</Link></p>
+                        <div className='text-center'>
+                            <p>Or sing in with</p>
+                            <div className='flex mt-2 gap-6 justify-center items-center'>
+                                <PiFacebookLogo className='text-4xl font-bold'></PiFacebookLogo><BsGoogle className='text-3xl font-bold'></BsGoogle><BsGithub className='text-3xl font-bold'></BsGithub>
+                            </div>
+                        </div>
                         </form>
                     </div>
                 </div>
